1fac65be80a2fdf0b3617743fb92bbc970fa5a63
yoavotm <yoavotmazgin@gmail.com>
2022-04-06 19:03:06 +0300

    =basic registration page

---
+++ Advanced2/web_app/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Registration from './registration/registration';
+
+function App() {
+    return ( 
+        <Registration/>
+    );
+}
+
+export default App;
---
+++ Advanced2/web_app/src/registration/registration.js
@@ -0,0 +1,20 @@
+function Registration() {
+    return (
+        <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-8">
+        <div class="mb-3">
+<label for="formGroupExampleInput" class="form-label">Example label</label>
+<input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder"> </input>
+</div>
+<div class="mb-3">
+<label for="formGroupExampleInput2" class="form-label">Another label</label>
+<input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder"></input>
+</div>
+</div>
+</div>
+    </div>
+    );
+}
+
+export default Registration
