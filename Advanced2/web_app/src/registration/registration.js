daf80c9152092f1a1704a9817d8aea4435c0cfa8
shahar <sasa0192837465@gmail.com>
2022-04-08 14:37:28 +0300

    adding to html and css registrion, should edit on css.

--- Advanced2/web_app/src/registration/registration.js
+++ Advanced2/web_app/src/registration/registration.js
@@ -1,19 +1,37 @@
 function Registration() {
     return (
-<div container>
-    <div class="mb-3 row">
-        <label for="Username" class="col-form-label">Username</label>
-        <div class="col-sm-3">
-            <input class="form-control" id="Username" placeholder="Israel Israeli"></input>
-        </div>
-    </div>
-    <div class="mb-3 row">
-        <label for="inputPassword" class="col-form-label">Password</label>
-        <div class="col-sm-3">
-            <input type="password" class="form-control" id="inputPassword"></input>
-        </div>
-    </div>
-</div>
+        <form>
+            <!-- Email input -->
+            <div class="form-outline mb-4">
+                <input type="email" id="form1Example1" class="form-control" />
+                <label class="form-label" for="form1Example1">Email address</label>
+            </div>
+
+            <!-- Password input -->
+            <div class="form-outline mb-4">
+                <input type="password" id="form1Example2" class="form-control" />
+                <label class="form-label" for="form1Example2">Password</label>
+            </div>
+
+            <!-- 2 column grid layout for inline styling -->
+            <div class="row mb-4">
+                <div class="col d-flex justify-content-center">
+                    <!-- Checkbox -->
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
+                        <label class="form-check-label" for="form1Example3"> Remember me </label>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <!-- Simple link -->
+                    <a href="#!">Forgot password?</a>
+                </div>
+            </div>
+
+            <!-- Submit button -->
+            <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+        </form>
     );
 }
 
