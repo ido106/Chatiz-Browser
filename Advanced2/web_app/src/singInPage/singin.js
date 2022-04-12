dceb75fcf61e20a1c2c6f73f71f25e9732efba84
shahar55 <sasa0192837465@gmail.com>
2022-04-12 13:48:13 +0300

    add validation

--- Advanced2/web_app/src/singInPage/singin.js
+++ Advanced2/web_app/src/singInPage/singin.js
@@ -1,42 +1,35 @@
+import React, { useState } from 'react';
+
 function submitsingin(data) {
 
 }
 
-export default function Singin() {
+export default function Singin({Login, error}) {
+    const [details, setDetails] = useState({userName:"", password:""});
+    const submitHandler = e => {
+      e.preventDefault();
+      Login(details);
+    }
     return (
-        <form onSubmit={submitsingin} className="boxForms">
-        {/* <!-- Email input --> */}
-        <div class="form-outline mb-4">
-            Username
-          <input type="text" id="Username" class="form-control" />
-        </div>
-      
-        {/* <!-- Password input --> */}
-        <div class="form-outline mb-4">
-            Password
-          <input type="password" id="password" class="form-control" />
-        </div>
-      
-        {/* <!-- 2 column grid layout for inline styling --> */}
-        <div class="row mb-4">
-          <div class="col d-flex justify-content-center">
-            {/* <!-- Checkbox --> */}
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="rememberme" checked />
-              <label class="form-check-label" for="rememberme"> Remember me </label>
-            </div>
+      <form onSubmit={submitHandler}>
+        <div className="boxForms">
+          <h2>
+            Login
+          </h2>
+          {(error !="") ? (<div className = "error">{error}</div>) :""}
+          <div></div>
+          <div className="form-group">
+            <label htmlFor="userName">user name</label>
+            <input type="text" name="userName" id="userName"
+            onChange={e =>setDetails({...details, userName: e.target.value})} value = {details.userName}/>
           </div>
-      
-          <div class="col">
-            {/* <!-- Simple link --> */}
-            Not register?&nbsp;
-            <a href="#!">Click here</a>
-            to register
+          <div className="form-group">
+            <label htmlFor="password">passsword</label>
+            <input type = "text" name = "password" id = "password"
+            onChange={e =>setDetails({...details, password: e.target.value})} value = {details.password}/>
           </div>
+          <input type = "submit" value="LOGIN"input/>
         </div>
-      
-        {/* <!-- Submit button --> */}
-        <button type="submit" class="btn btn-primary btn-block">Sign in</button>
       </form>
     )
 }
