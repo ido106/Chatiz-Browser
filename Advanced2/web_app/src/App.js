386b3bfbb091f4ed81caf51d26c989b1e2606823
yoavotm <yoavotmazgin@gmail.com>
2022-04-12 21:17:45 +0300

    minor changes

--- Advanced2/web_app/src/App.js
+++ Advanced2/web_app/src/App.js
@@ -1,14 +1,43 @@
 import Singin from './singInPage/singin';
 import Registration from './registration/registration';
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-  } from "react-router-dom";
+import React, { useState } from 'react';
 
 function App() {
-    return (
-        <Singin/>
+    const adminUser = {
+        userName: "otma",
+        password: "homo"
+    }
+    const [user, setUser] = useState({ userName: "", password: "" });
+    const [error, setError] = useState("")
+    const Login = details => {
+        console.log(details);
+        if (details.userName == adminUser.userName && details.password == adminUser.password) {
+            console.log("login...");
+            setUser({
+                userName: details.userName, password: details.password
+            })
+        } else {
+            console.log("detailsDoNotMutch");
+            setError("details do not match");
+        }
+    }
+    const Logout = () => {
+        console.log("logout");
+        setUser({userName:"", password:""});
+    }
+    return ( 
+        <div>
+            {(user.userName!="") ? (
+                <div className='welcome'>
+                    <h2>
+                        welcome, <span>{user.userName}</span></h2>
+                    <button onClick={Logout}>Logout</button>
+                </div>
+            ):
+            (
+                <Singin Login = {Login} error = {error}/>
+            )}
+        </div>
     );
 }
 
