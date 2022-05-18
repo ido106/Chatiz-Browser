d9957de2602f8045a1ca2671b19b343dd6cd8b9c
Ido Aharon <idoah106@gmail.com>
2022-05-18 18:29:21 +0300

    otma tembel tikun

--- Advanced2_ASP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Advanced2_ASP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
